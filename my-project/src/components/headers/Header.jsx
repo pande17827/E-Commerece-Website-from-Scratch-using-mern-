d3ea274a26159285d07ac4678d8c08b360bdd4d5
Vikas Kumar <45943546+pande17827@users.noreply.github.com>
2025-01-17 13:12:27 -0800

    Updated all files

--- my-project/src/components/headers/Header.jsx
+++ my-project/src/components/headers/Header.jsx
@@ -1,14 +1,14 @@
-import styles from './Header.module.css';
+import style from './Header.module.css';
 
 function Header() {
   return (
     <>
       <div className={`${style.container} ${style.bg_color}`}>
-        <div className={styles.container}>
-          <div className={styles.flex}>
-            <div className={styles.col1}>
-              <p className={styles.textSmall}>
-                Get up to 50% off new season styles, limited time only
+        <div className={style.container}>
+          <div className={style.flex}>
+            <div className={style.col1}>
+              <p className={style.textSmall}>
+                Get up to 50% off new season style, limited time only
               </p>
             </div>
           </div>
