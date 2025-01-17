2dab95e665980fef91bb0de4379bfdc8ef96f947
Vikas Kumar <45943546+pande17827@users.noreply.github.com>
2025-01-17 20:20:51 +0000

    Updated all files

--- my-project/src/components/headers/Header.jsx
+++ my-project/src/components/headers/Header.jsx
@@ -1,7 +1,7 @@
 function Header(){
     return (
         <>
-           <h1>pandey</h1>
+           
            <div className="top_strip">
                 <div className="container">
                     <div className="flex items-center justify-between">
