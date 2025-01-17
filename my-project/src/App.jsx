29f80c7b89784c5b807ba131461d39462ebefefe
Vikas Kumar <45943546+pande17827@users.noreply.github.com>
2025-01-17 19:49:28 +0000

    Updated all files

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,8 +2,9 @@ import Header from "./components/headers/Header.jsx"
 
 export default function App() {
   return (
-    <h1 className="text-{50px} bg-slate-500">
-      Hello world!
-    </h1>
+    <>
+    <Header></>
+    
+    </>
   )
 }
