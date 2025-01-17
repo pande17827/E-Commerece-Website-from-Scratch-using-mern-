94d0f111a9a2094b8499d37fdf8c4a6dee20980d
Vikas Kumar <45943546+pande17827@users.noreply.github.com>
2025-01-17 20:21:22 +0000

    Updated all files

--- my-project/src/components/headers/Header.jsx
+++ my-project/src/components/headers/Header.jsx
@@ -2,7 +2,7 @@ function Header(){
     return (
         <>
            
-           <div className="top_strip">
+           <div className="top_strip py-2">
                 <div className="container">
                     <div className="flex items-center justify-between">
                         <div className="col1 w-[50%]">
